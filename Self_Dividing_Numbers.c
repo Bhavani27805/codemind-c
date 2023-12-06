df2a720efd5b752c93af85f6b39de03fac1accaa
Bhavani27805 <147792075+Bhavani27805@users.noreply.github.com>
2023-12-06 09:53:34 +0530

    #include<stdio.h> int main() {     int n1,n2,s=0,i,m,a;     scanf("%d%d",&n1,&n2);     for(i=n1;i<=n2;i++)     {         int a=i;m=0;     while(a)     {         m=a%10;     if(m==0)         {             s=0;             break;         }     if(i%m!=0)         {             s=0;             break;         }     if(i%m==0)         {             s=1;         }         a=a/10;     }     if(s==1)     {         printf("%d ",i);     }     } }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,s=0,i,m,a;
+    scanf("%d%d",&n1,&n2);
+    for(i=n1;i<=n2;i++)
+    {
+        int a=i;m=0;
+    while(a)
+    {
+        m=a%10;
+    if(m==0)
+        {
+            s=0;
+            break;
+        }
+    if(i%m!=0)
+        {
+            s=0;
+            break;
+        }
+    if(i%m==0)
+        {
+            s=1;
+        }
+        a=a/10;
+    }
+    if(s==1)
+    {
+        printf("%d ",i);
+    }
+    }
+}
